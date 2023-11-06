b58261b5db68b34bd8046b5bf6ae31f1fdec8186
feelsuegood <suaj2021@gmail.com>
2023-11-06 20:10:18 +1000

    .

--- test.py
+++ test.py
@@ -12,14 +12,14 @@ load_dotenv()
 
 
 url = os.getenv('URL_LB')
-COCURRENT_REQUESTS = 2  # Max concurrent requests
+CONCURRENT_REQUESTS = 5  # Max concurrent requests
 ITERATION_REQUESTS = 100000  # Number of iterations
 DELAY = 2  # Delay between requests in seconds
 TIMEOUT = 10  # POST request timeout in seconds
 RETRIES = 1  # Number of retries
 MAX_ITERATION = 100  # Max threads
 # Test maximum file size
-FILE = 'request/test-10mb.jpg'  # Image file for upload
+FILE = 'test-img/test-10mb.jpg'  # Image file for upload
 WIDTH = 1920  # Image width
 HEIGHT = 1080  # Image height
 FORMAT = 'JPEG'  # Image format
@@ -107,4 +107,4 @@ def perform_multiple_requests(num_threads: int, num_iterations_per_thread: int, 
 if __name__ == "__main__":
     # for _ in range(100):
     perform_multiple_requests(
-        COCURRENT_REQUESTS, ITERATION_REQUESTS, DELAY)
+        CONCURRENT_REQUESTS, ITERATION_REQUESTS, DELAY)
